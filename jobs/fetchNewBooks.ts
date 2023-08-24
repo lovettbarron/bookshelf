eb2086e48db819c0f6176564a66dce6d63a585d9
andrewlb <alb@andrewlb.com>
2023-08-24 15:10:36 +0200

    Adding logic

--- jobs/fetchNewBooks.ts
+++ jobs/fetchNewBooks.ts
@@ -1,5 +1,32 @@
 import { FetchNewBooks, models } from '@teamkeel/sdk';
+import { CommonTableExpressionNameNode } from 'kysely';
 export default FetchNewBooks(async (ctx) => {
-	// Build something cool
+
 });
-	
+
+const SearchISBN = (isbn) => {
+	const books_api_key = "AIzaSyA_9-GbYiVfbwRJEmDwXC8QY-S3OW-k9to"
+	const books_endpoint = (isbn) => `https://www.googleapis.com/books/v1/volumes?q=isbn+` + isbn;
+
+	const handler = (response) => {
+		const res = response;
+		return {	
+			name: res.name,
+			author:res.author,
+			published: res.published
+		};
+	};
+
+
+	const data = fetch(books_endpoint, {
+		method: "POST",
+		body: JSON.stringify({ name: "Simon" }),
+	}).then((res) => handler(res));
+
+	return {
+		name: data.name,
+		author:data.author,
+		published: data.published
+
+	}
+}
