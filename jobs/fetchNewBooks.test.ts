1c801f123727497a3ce9d3e56bf58b0aedcd189c
andrewlb <alb@andrewlb.com>
2023-08-24 21:53:07 +0200

    sts

--- jobs/fetchNewBooks.test.ts
+++ jobs/fetchNewBooks.test.ts
@@ -4,12 +4,22 @@ import { test, expect, beforeEach } from "vitest";
  
 beforeEach(resetDatabase);
  
-test("Test FetchNewBooks updates", async () => {
+
+test('a permission test', async () => {
+  const identity = await models.identity.create({
+    email: 'andrew@keel.xyz',
+  });
+    
+actions.withIdentity(identity).createBook({isbn:"9781844164592"})
+
+
+
   console.log("Test start")
  
   await jobs.fetchNewBooks();
  
   const res = await models.book.findMany({where: { title: null }	});
   console.log(res)
+
   expect(res).toBeLessThan(1);
 });
